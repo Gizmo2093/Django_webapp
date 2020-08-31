ee15def50606d3aac522f4f512b529171d9398eb
Gizmo2093 <gizmo2093@gmail.com>
2020-08-31 15:06:56 +0700

    Fourth commit
    Creating admin-panel
    Creating superuser
    Creating model "Entry" for text in topic

--- venv/project/blogs/models.py
+++ venv/project/blogs/models.py
@@ -8,3 +8,17 @@ class Topic(models.Model):
     def __str__(self):
         """return string representation"""
         return self.text
+
+class Entry(models.Model):
+    """Entry in Post """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + "..."
+
+
