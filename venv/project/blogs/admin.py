fedd2df38e124c2e03fb4f854c856cb7d794a307
Gizmo2093 <gizmo2093@gmail.com>
2020-09-01 13:00:42 +0700

    thith commit
    work from urls and views

--- venv/project/blogs/admin.py
+++ venv/project/blogs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from blogs.models import Topic
+from blogs.models import Topic, Entry
 admin.site.register(Topic)
+admin.site.register(Entry)
--- venv/project/project/urls.py
+++ venv/project/project/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    urls(r'', include('blogs.urls', namespace = 'blogs')),
 ]
